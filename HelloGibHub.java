197fcbc1e0e02e8b5e6e19be99818eeaf33b643e
Lucy <lucykwang@Keyings-MacBook-Pro.local>
2019-02-26 20:27:49 -0500

    commit message

--- HelloGibHub.java
+++ HelloGibHub.java
@@ -1,6 +1,6 @@
 public class HelloGibHub {
 	
 	public static void main (String [] commandLine) {
-		System.out.println( "Hello, GibHub");
+		System.out.println( "Hello, GibHubbers"); //improved version
 	}
 }
