75983751feeb58bcb395580fd8131c4f3a1b6698
Lucy <kwang90@stuy.edu>
2019-02-28 13:07:00 -0500

    commit message

--- HelloGibHub.java
+++ HelloGibHub.java
@@ -1,6 +1,9 @@
 public class HelloGibHub {
 	
 	public static void main (String [] commandLine) {
-		System.out.println( "Hello, GibHubbers"); //improved version
+		System.out.println( "Hello, GibHubbers around the world!"); //improved version
 	}
 }
+//Edited on Stuy computer
+
+
